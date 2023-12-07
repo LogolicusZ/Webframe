80a4b95648e365922a1435e47231bcdb10378654
LogolicusZ <adjordbra@gmail.com>
2023-12-07 21:16:58 +0100

    changes

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,24 +1,12 @@
+<script lang="ts">
+	import Gallery from './Gallery.svelte';
+
+</script>
+
 <div class="container">
-	<content>
-		<section class="section-1">
-			<div class="landing">
-				<div style="grid-area: 🟥;" id="hey">
-					<p>Hey I'm</p>
-				</div>
-	
-				<div style="grid-area: 🟩;" id="name">
-					<p>LogolicusZ</p>
-				</div>
-	
-				<div style="grid-area: 🟪;" id="arrow">
-					<img class="logo" src="assets/down-arrow-svgrepo-com.svg" alt="LogolicusZ Logo" />
-				</div>
-			</div>
-		</section>
-	</content>
+	<Gallery />
 </div>
 
 <style lang="scss">
 	@import '../../app.scss';
-	@import '../../styles/home.scss';
 </style>
