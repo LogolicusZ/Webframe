96227d7d8d3e5dfc5ee52acfa0bd4f418372593e
LogolicusZ <adjordbra@gmail.com>
2023-12-07 18:28:21 +0100

    chanfes

---
+++ src/routes/gallery/Gallery.svelte
@@ -0,0 +1,11 @@
+<body>
+	<section id="section-1">
+		<div class="Gallery">
+		</div>
+	</section>
+</body>
+
+<style lang="scss">
+	@import '../../app.scss';
+	@import '../../styles/gallery.scss';
+</style>
