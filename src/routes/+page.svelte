561edd8a331b3cb2905249154bcd3d62b42985cb
LogolicusZ <adjordbra@gmail.com>
2023-12-11 09:10:27 +0100

    changes clean ups

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,12 @@
 	import Home from './Home.svelte';
 	import Text from './Text.svelte';
 	import About from './About.svelte';
-	import Preview from './Preview.svelte';
 </script>
 
 <div class="container">
 	<Home />
 	<Text />
 	<About />
-	<Preview />
 </div>
 
 <style lang="scss">
