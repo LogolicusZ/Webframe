e9b4645f4bbb5ac102b8eb57fb564d73859c5666
LogolicusZ <adjordbra@gmail.com>
2023-12-16 10:23:46 +0100

    gallery mobile

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -6,7 +6,7 @@
 					<img src="/icons/klammer1.svg" alt="circle" class="light" />
 					<img src="/icons/klammer1dark.svg" alt="circle" class="dark" />
 				</div>
-				<div class="titlegallery">
+				<div class="title-gallery">
 					<p>Gallery</p>
 				</div>
 				<div id="addon2">
@@ -14,54 +14,56 @@
 					<img src="/icons/klammer1dark.svg" alt="circle" class="dark1" />
 				</div>
 			</div>
-			<div class="Gallery">
-				<div id="space"></div>
+			<div class="Gallery-contaienr">
+				<div class="Gallery">
+					<div id="space"></div>
 
-				<div class="img1">
-					<img src="/assets/img/picture1.png" alt="1" />
+					<div class="img1">
+						<img src="/assets/img/picture1.png" alt="1" />
+					</div>
+
+					<div class="img2">
+						<img src="/assets/img/picture2.png" alt="2" />
+					</div>
+
+					<div id="space"></div>
+
+					<div id="space"></div>
+
+					<div class="img3">
+						<img src="/assets/img/picture3.png" alt="3" />
+					</div>
+
+					<div class="img4">
+						<img src="/assets/img/picture4.png" alt="4" />
+					</div>
+
+					<div id="space"></div>
+
+					<div id="space"></div>
+
+					<div class="img5">
+						<img src="/assets/img/picture5.png" alt="5" />
+					</div>
+
+					<div class="img6">
+						<img src="/assets/img/picture6.jpg" alt="6" />
+					</div>
+
+					<div id="space"></div>
+
+					<div id="space"></div>
+
+					<div class="img7">
+						<img src="/assets/img/picture7.jpg" alt="7" />
+					</div>
+
+					<div class="img8">
+						<img src="/assets/img/picture8.jpg" alt="8" />
+					</div>
+
+					<div id="space"></div>
 				</div>
-
-				<div class="img2">
-					<img src="/assets/img/picture2.png" alt="2" />
-				</div>
-
-				<div id="space"></div>
-
-				<div id="space"></div>
-
-				<div class="img3">
-					<img src="/assets/img/picture3.png" alt="3" />
-				</div>
-
-				<div class="img4">
-					<img src="/assets/img/picture4.png" alt="4" />
-				</div>
-
-				<div id="space"></div>
-
-				<div id="space"></div>
-
-				<div class="img5">
-					<img src="/assets/img/picture5.png" alt="5" />
-				</div>
-
-				<div class="img6">
-					<img src="/assets/img/picture6.jpg" alt="6" />
-				</div>
-
-				<div id="space"></div>
-
-				<div id="space"></div>
-
-				<div class="img7">
-					<img src="/assets/img/picture7.jpg" alt="7" />
-				</div>
-
-				<div class="img8">
-					<img src="/assets/img/picture8.jpg" alt="8" />
-				</div>
-
-				<div id="space"></div>
 			</div>
 		</section>
 	</body>
