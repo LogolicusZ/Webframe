906cfdca129f390c28043a6913dde955b35fdde7
LogolicusZ <adjordbra@gmail.com>
2024-01-07 18:36:02 +0100

    revert to saved commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-=======
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
->>>>>>> parent of b0ab2c4 (Pre pages)
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,25 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-		pages: 'build',
-		assets: 'build',
-		fallback: '404.html',
-		precompress: false,
-		strict: true,
-		prerender: {
-			handleMissingId: (href, page) => {
-			  // Ignore errors for '/#top' on any page
-			  return href === '#top';
-			},
-		},
-	
-	}),
-	paths: {
-		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-	}
-		
-
+		adapter: adapter()
 	}
 };
 
