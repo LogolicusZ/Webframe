864b55923fbaf16220ae1e10c131e5b2d37f4014
LogolicusZ <adjordbra@gmail.com>
2024-01-10 09:26:44 +0100

    Choosed right style of cards, added light and dark mode for Design Tab. - NOT FOR RELEASE

--- src/routes/Designs.svelte
+++ src/routes/Designs.svelte
@@ -1,67 +1,58 @@
 <body>
-	<section id="section-6"	class="section-6">
+	<section id="section-6" class="section-6">
 		<div class="Title">
 			<div id="addon1">
-				<img src="/icons/star.svg" alt="circle" class="light" />
+				<img src="/icons/starweirdlight.svg" alt="circle" class="light" />
 				<img src="/icons/Starweird.svg" alt="circledark" class="dark" />
 			</div>
 			<div class="title1">
 				<h1>Designs</h1>
 			</div>
 			<div id="addon2">
-				<img src="/icons/star.svg" alt="circle" class="light1" />
-				<img src="/icons/Starweird.svg" alt="circledark" class="dark1"/>
+				<img src="/icons/starweirdlight.svg" alt="circle" class="light1" />
+				<img src="/icons/Starweird.svg" alt="circledark" class="dark1" />
 			</div>
 		</div>
-		<div class="design-container">
-			<div class="cardcontainer">
-				<div class="card">
-				<div class="content">
-					<img class="logo" src="favicon.svg" alt="mparticle">
-					<h6>Data &amp; Infrastructure Agility</h6>
-					<div class="hover_content">
-					<p>mParticle’s customer data platform empowers you to Integrate all of your data and orchestrate it across channels, partners, and systems.</p>
-					</div>
+		<div class="container">
+			<div class="box">
+				<img src="assets/logodark.svg" class="logodark" />
+				<img src="assets/logo1.svg" class="logo" />
+				<div>
+					<strong>PERSONAL LOGO</strong>
+					<span>CURRENT</span> <span>09/23</span>
 				</div>
-				</div>       
 			</div>
-			<div class="cardcontainer">
-				<div class="card">
-				<div class="content">
-					<img class="logo" src="favicon.svg" alt="mparticle">
-					<h6>Data &amp; Infrastructure Agility</h6>
-					<div class="hover_content">
-					<p>mParticle’s customer data platform empowers you to Integrate all of your data and orchestrate it across channels, partners, and systems.</p>
+			<a class="dashinit-link"  href="https://gitpot.dev/dashinit/cli">
+				<div class="box">
+					
+						<img src="assets/dashinitdark.svg" class="dashinitdark" />
+						<img src="assets/dashinitlight.svg" class="dashinit" />
+				
+					<div class="desc">
+						<strong>DASHINIT</strong>
+						<span>CURRENT</span> <span>09/23</span>
 					</div>
 				</div>
-				</div>       
+			</a>
+			<div class="box">
+				<img src="assets/old23dark.svg" class="old23dark" />
+				<img src="assets/old23light.svg" class="old23" />
+				<div>
+					<strong>PERSONAL LOGO</strong>
+					<span>OUTDATED</span> <span>04/23</span>
+				</div>
 			</div>
-			<div class="cardcontainer">
-				<div class="card">
-				<div class="content">
-					<img class="logo" src="favicon.svg" alt="mparticle">
-					<h6>Data &amp; Infrastructure Agility</h6>
-					<div class="hover_content">
-					<p>mParticle’s customer data platform empowers you to Integrate all of your data and orchestrate it across channels, partners, and systems.</p>
-					</div>
+			<div class="box">
+				<img src="assets/old22dark.svg" class="old22dark" />
+				<img src="assets/22oldlight.svg" class="old22" />
+				<div>
+					<strong>PERSONAL LOGO</strong>
+					<span>OUTDATED</span> <span>12/22</span>
 				</div>
-				</div>       
-			</div>
-			<div class="cardcontainer">
-				<div class="card">
-				<div class="content">
-					<img class="logo" src="favicon.svg" alt="mparticle">
-					<h6>Data &amp; Infrastructure Agility</h6>
-					<div class="hover_content">
-					<p>mParticle’s customer data platform empowers you to Integrate all of your data and orchestrate it across channels, partners, and systems.</p>
-					</div>
-				</div>
-				</div>       
 			</div>
 		</div>
 	</section>
 </body>
-
 
 <style lang="scss">
 	@import '../app.scss';
--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -2,14 +2,14 @@
 	<section id="section-7"	class="section-7">
 		<div class="Title">
 			<div id="addon1">
-				<img src="/icons/star.svg" alt="circle" class="light" />
+				<img src="/icons/cleanstarlight.svg" alt="circle" class="light" />
 				<img src="/icons/cleanstar.svg" alt="circledark" class="dark" />
 			</div>
 			<div class="title1">
 				<h1>Projects</h1>
 			</div>
 			<div id="addon2">
-				<img src="/icons/star.svg" alt="circle" class="light1" />
+				<img src="/icons/cleanstarlight.svg" alt="circle" class="light1" />
 				<img src="/icons/cleanstar.svg" alt="circledark" class="dark1"/>
 			</div>
 		</div>
