592422c2ad7b70ae2171763aaa3f7ffd664c4c38
LogolicusZ <adjordbra@gmail.com>
2024-01-24 15:45:52 +0100

    Updated Vite and Sveltekit, Deactivated Designs and Projects section.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,16 @@
 	import Home from './Home.svelte';
 	import Text from './Text.svelte';
 	import About from './About.svelte';
-	import Designs from './Designs.svelte';
-	import Projects from './Projects.svelte';
+	// import Designs from './Designs.svelte';
+	// import Projects from './Projects.svelte';
 </script>
 
 <div class="container">
 	<Home />
 	<Text />
 	<About />
-	<Designs />
-	<Projects />
+	<!-- <Designs />
+	<Projects /> -->
 </div>
 
 <style lang="scss">
