d87ebad470af9608a34723bdf976e53fe5183ea2
LogolicusZ <adjordbra@gmail.com>
2024-07-08 22:16:29 +0200

    [c] updated Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.scss';
     import '../styles/Mobile/app-mobile.scss';
+	import { onMount } from 'svelte';
 
 	import { fly } from 'svelte/transition';
 	export let data;
@@ -26,25 +27,38 @@
 			scrollToTop();
 		}
 	}
+
+
+	onMount(() => {
+  // check if the nav is scrolled, and if yes, add the .scrolled class
+  window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    if (navbar) {
+      if (window.scrollY > 0) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    }
+    // If navbar is null, the code will simply skip the interior of the if block
+  });
+});
 </script>
-<div class="nav-container" id="top">
-    <div class="nav">
-        <div class="nav-logo">
-            <a class="logo-link"  href="/">
-                <img class="logo" src="assets/logo1.svg" alt="LogolicusZ Logo" />
-				<img class="logodark" src="assets/logodark.svg" alt="LogolicusZ Logo" />
-            </a>
-        </div>
-        <div class="nav-items">
-            <a href="/#section-3">
-                <button class="about">About</button>
-            </a>
-            <a href="/gallery">
-                <button class="gallery">Gallery</button>
-            </a>
-        </div>
+
+
+<header id="navbar">
+    <div class="logo-container">
+        <img id="logo1" src="/assets/heyo-main.svg" alt="Heyo Logo">
+        <img id="logo2" src="/logolicusz.svg" alt="Logo">
     </div>
-</div>
+    <nav>
+        <ul>
+            <li><a href="/#section-3">About</a></li>
+			<li><a href="https://blog.logolicusz.com">Blog</a></li>
+            <li><a href="/gallery">Gallery</a></li>
+        </ul>
+    </nav>
+</header>
 
 {#key data.pathname}
 	<main in:fly={{ x: -10, duration: 250, delay: 125 }} out:fly={{ x: 5, duration: 125 }}>
